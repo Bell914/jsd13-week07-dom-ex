ce4fe5169e8b6aacdc005f50d751e7557d00b17d
Bell914 <ittikorn.tipson@gmail.com>
2026-08-11 11:39:11 +0700

    feat: randomize Pokemon selection on button click

--- jsd13-dom-ex/pokemon/script.js
+++ jsd13-dom-ex/pokemon/script.js
@@ -2,7 +2,9 @@ const pokedex = document.getElementById('pokedex');
 const btnPokemon = document.getElementById('btn-pokemon');
 
 btnPokemon.addEventListener('click', async()=>{
-    const findPokemon = await fetch('https://pokeapi.co/api/v2/pokemon/67'); //รอเพื่อรอจับข้อมูลมาที่ฝั่ง
+    // const findPokemon = await fetch('https://pokeapi.co/api/v2/pokemon/67'); //รอเพื่อรอจับข้อมูลมาที่ฝั่ง
+    const randomId = Math.floor(Math.random() * 898) + 1;// สุ่มเลขจำนวนเต็มระหว่าง 1 ถึง 898 (จำนวนโปเกมอนทั้งหมดใน PokeAPI)
+    const findPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);// นำเลขที่สุ่มได้ไปแทรกใน URL เพื่อดึงข้อมูลโปเกมอนตัวนั้นแบบสุ่ม
     const data = await findPokemon.json(); //รอเพื่อแปลงข้อมูลมาเป็น json 
     console.log(data); //เพิ่มค่า
     //pokedex.innerHTML = `${data.name} <img src="${data.sprites.front_default}">`;
