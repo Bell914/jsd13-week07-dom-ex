ca55de30870aed0447e874615e1f2cdc0a5f898b
Bell914 <ittikorn.tipson@gmail.com>
2026-08-11 13:26:30 +0700

    feat: add card styling and click-to-remove functionality

--- jsd13-dom-ex/pokemon/script.js
+++ jsd13-dom-ex/pokemon/script.js
@@ -1,14 +1,21 @@
+// random pokemon 
+// add name
+// remove when click 
+// add some styles to that
+
 const pokedex = document.getElementById('pokedex');
 const btnPokemon = document.getElementById('btn-pokemon');
 
 btnPokemon.addEventListener('click', async()=>{
     // const findPokemon = await fetch('https://pokeapi.co/api/v2/pokemon/67'); //รอเพื่อรอจับข้อมูลมาที่ฝั่ง
-    const randomId = Math.floor(Math.random() * 898) + 1;// สุ่มเลขจำนวนเต็มระหว่าง 1 ถึง 898 (จำนวนโปเกมอนทั้งหมดใน PokeAPI)
+    const randomId = Math.floor(Math.random() * 898) + 1;// สุ่มเลขจำนวนเต็มระหว่าง 1 ถึง 898 จำนวนโปเกมอนทั้งหมดใน PokeAPI
     const findPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);// นำเลขที่สุ่มได้ไปแทรกใน URL เพื่อดึงข้อมูลโปเกมอนตัวนั้นแบบสุ่ม
-    const data = await findPokemon.json(); //รอเพื่อแปลงข้อมูลมาเป็น json 
+    const data = await findPokemon.json(); // รอเพื่อแปลงข้อมูลมาเป็น json 
+    const div = document.createElement('div');
+    div.classList.add('pokemon-card'); // ให้ div ตัวที่ใช้งานจริงมี class
     console.log(data); //เพิ่มค่า
     //pokedex.innerHTML = `${data.name} <img src="${data.sprites.front_default}">`;
-    const div = document.createElement('div'); //สร้างกล่อง div
+    
     const name = document.createElement('p');
     const img = document.createElement('img');
     name.textContent = data.name;
@@ -16,4 +23,9 @@ btnPokemon.addEventListener('click', async()=>{
     div.append(name);
     div.append(img);
     pokedex.append(div); //ทุกครั้งที่กันสร้าง มันจะสร้างขึ้นมาใหม่เพื่อรวนอันอีกครั้ง
+
+    div.addEventListener('click', () => {
+        div.remove();
+    });
+
 });
